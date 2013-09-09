98c40696043874346b9308d41027ad93648e4408
Bryce Handerson <bryce.handerson@gmail.com>
2013-09-09 09:19:05 -0700

    added comments

--- a3.help/bc.c
+++ a3.help/bc.c
@@ -36,7 +36,8 @@ u16 getblk(u16 blk, char *buf)
 	readfd( blk/18, ((blk*2)%36)/18, ((blk*2)%36)%18, buf);
 }
 
-u16 find(u16 block, char *name){
+u16 find(u16 block, char *name)
+{
 	DIR *dirp;
 	char *c;
 	getblk(block, buf2);
@@ -54,7 +55,8 @@ u16 find(u16 block, char *name){
 	return 0;
 }
 
-INODE *gino(u16 blk, u16 node){
+INODE *gino(u16 blk, u16 node)
+{
 	u16 bk, id;
 	bk = blk + ((node -1) / 8);
 	id = (node - 1) % 8;
@@ -73,45 +75,40 @@ main()
 	DIR   *dp;
 	u32   *bp;
 
+	// load filesystem
 	getblk(2, buf1);
 	gp = (GD *)buf1;
 	iblk = (u16)gp->bg_inode_table;
 
+	// prompt for boot image
 	myprints("img: ");
 	mygets(temp);
 
 	getblk((u16)iblk, buf1);  // read first inode block block
 	ip = (INODE *)buf1 + 1;   // ip->root inode #2
+
+	// get boot dir inode
 	i = find((u16)ip->i_block[0], "boot");
-	// i = 13
 	ip = gino(iblk, i);
+
+	// get boot image inode
 	i = find((u16)ip->i_block[0], temp);
-	// i = 34
-	//putc(i+'0');
-	//getc();
-
 	ip = gino(iblk, i);
 
+	// load boot image into buf2
 	getblk((u16)ip->i_block[12], buf2);
+
+	// set ES register to 1024k
 	setes(0x1000);
+
+	// load direct blocks
 	for (i=0; i < 12 && ip->i_block[i] != 0; i ++){
 		getblk((u16)ip->i_block[i], (i*1024));
-		putc('-');
 	}
 
+	// load indirect blocks
 	for (i=0, bp=buf2; i < 256 && bp[i] > 0; i++){
 		getblk((u16)bp[i], (i+12)*1024);
-		putc('.');
 	}
-	getc();
-	return 1;/*
-	while((char *)dp < &buf2[BLK]){
-		c = dp->name[dp->name_len];
-		dp->name[dp->name_len] = 0;
-		myprints(dp->name); putc(' ');
-		dp->name[dp->name_len] = c;
-		dp = (DIR *)((char *)dp + dp->rec_len);
-	}
-
-	myprints("\n\rgo?"); getc();*/
+	return 1;
 }
